dcce1783705d932f5a0a037b6d712a16716dd483
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-24 17:39:25 +0100

    added input reading to output custom game states

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print("""
-X O X
-O X O
-X X O
-""")
+combined_string = input()
+line_1 = combined_string[0:3]
+line_2 = combined_string[3:6]
+line_3 = combined_string[6:9]
+print("---------")
+print("|", line_1[0], line_1[1], line_1[2], "|")
+print("|", line_2[0], line_2[1], line_2[2], "|")
+print("|", line_3[0], line_3[1], line_3[2], "|")
+print("---------")
