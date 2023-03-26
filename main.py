23df2fec42e16c75a2ec635b6259c40dabc9b1e9
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-26 16:42:06 +0200

    added formatting to the project

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Description: Tic-Tac-Toe game in Python
+
 combined_string = input()
 line_1 = combined_string[:3]
 line_2 = combined_string[3:6]
@@ -14,15 +16,38 @@ print("|", line_1[0], line_1[1], line_1[2], "|")
 print("|", line_2[0], line_2[1], line_2[2], "|")
 print("|", line_3[0], line_3[1], line_3[2], "|")
 print("---------")
+
+
 def check_win(symbol):
-    if symbol * 3 in [line_1, line_2, line_3, column_1, column_2, column_3, diagonal_1, diagonal_2]:
+    """_check_win_
+    Args:
+        symbol (_string_): _player symbol_ (default: {_})
+
+    Returns:
+        _bool_: _if symbol won or not_
+    """
+    if symbol * 3 in [
+        line_1,
+        line_2,
+        line_3,
+        column_1,
+        column_2,
+        column_3,
+        diagonal_1,
+        diagonal_2,
+    ]:
         return True
     else:
         return False
 
-if (abs(combined_string.count(symbols[0]) - combined_string.count(symbols[1])) > 1) or (check_win("X") and check_win("O")):
+
+if (abs(combined_string.count(symbols[0]) - combined_string.count(symbols[1])) > 1) or (
+    check_win("X") and check_win("O")
+):
     print("Impossible")
-elif (not check_win("X") and not check_win("O")) and ("_" or " ") not in combined_string:
+elif (not check_win("X") and not check_win("O")) and (
+    "_" or " "
+) not in combined_string:
     print("Draw")
 elif check_win(symbols[0]):
     print(symbols[0], "wins")
@@ -30,4 +55,3 @@ elif check_win(symbols[1]):
     print(symbols[1], "wins")
 else:
     print("Game not finished")
-    
